96aa8dc032d1ca117e1db243b7cf47e10e3ec4b0
Lindsay Kinn <lindsay.kinn@gmail.com>
2020-11-11 18:48:14 -0500

    commit tree 46a08a6e9a9d90a8e603c36fe998460662b6391c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+if array.size<3
+    return array.join (" and ")
+end
+
+if array.size >= 3
+    big_list = array [0..-2].join(", ") 
+    big_list << ", and" #pushes the oxford comma and `and` onto the array
+    big_list << " #{array[-1]}" #pushes last array item back onto the array
+    return big_list
+   
+end 
+
 
 end
